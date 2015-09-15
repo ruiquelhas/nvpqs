a61b04bb3fc0854f71f04385d7c5acd7332dee38
Rui Quelhas <rfpquelhas@gmail.com>
2015-09-15 21:03:33 +0100

    Update lab settings and add more test scripts.
    * Add support for generating a tap file.
    * Add support for generating an html report.
    * Add code coverage and linting requirements.
    * Fix linting issues.

--- test/index.js
+++ test/index.js
@@ -1,16 +1,16 @@
 'use strict';
 
-var qs = require('querystring');
+var Qs = require('querystring');
 
 var Lab = require('lab');
 var Code = require('code');
 
 var lab = exports.lab = Lab.script();
 
-var nvpqs = require('../');
+var Nvpqs = require('../');
 
-var nvp_stub = require('./stubs/nvp.json');
-var object_stub = require('./stubs/object.json');
+var NVPStub = require('./stubs/nvp.json');
+var ObjectStub = require('./stubs/object.json');
 
 lab.experiment('NVP encoded querystring utility belt', function () {
 
@@ -19,7 +19,7 @@ lab.experiment('NVP encoded querystring utility belt', function () {
         lab.test('returns empty object when the querystring is blank', function (done) {
 
             var input = '';
-            var output = nvpqs.parse(input);
+            var output = Nvpqs.parse(input);
 
             Code.expect(output).to.be.an.object();
             Code.expect(output).to.be.empty();
@@ -29,10 +29,10 @@ lab.experiment('NVP encoded querystring utility belt', function () {
 
         lab.test('returns matching object when the querystring is not blank and valid', function (done) {
 
-            var output = nvpqs.parse(qs.stringify(nvp_stub));
+            var output = Nvpqs.parse(Qs.stringify(NVPStub));
 
             Code.expect(output).to.be.an.object();
-            Code.expect(output).to.deep.equal(object_stub);
+            Code.expect(output).to.deep.equal(ObjectStub);
 
             return done();
         });
@@ -43,7 +43,7 @@ lab.experiment('NVP encoded querystring utility belt', function () {
         lab.test('returns a blank querystring if the object is empty or null', function (done) {
 
             var input = {};
-            var output = nvpqs.stringify(input);
+            var output = Nvpqs.stringify(input);
 
             Code.expect(output).to.be.a.string();
             Code.expect(output).to.be.empty();
@@ -53,10 +53,10 @@ lab.experiment('NVP encoded querystring utility belt', function () {
 
         lab.test('returns a matching querystring if the object is not empty nor null', function (done) {
 
-            var output = nvpqs.stringify(object_stub);
+            var output = Nvpqs.stringify(ObjectStub);
 
             Code.expect(output).to.be.a.string();
-            Code.expect(output).to.equal(qs.stringify(nvp_stub));
+            Code.expect(output).to.equal(Qs.stringify(NVPStub));
 
             return done();
         });
@@ -66,70 +66,70 @@ lab.experiment('NVP encoded querystring utility belt', function () {
 
         lab.test('returns a matching querystring using a custom delimiter character', function (done) {
 
-            var output = nvpqs.stringify(object_stub, '-');
+            var output = Nvpqs.stringify(ObjectStub, '-');
 
             Code.expect(output).to.be.a.string();
-            Code.expect(output).to.equal(qs.stringify(nvp_stub).replace(/\./g, '-'));
+            Code.expect(output).to.equal(Qs.stringify(NVPStub).replace(/\./g, '-'));
 
             return done();
         });
 
         lab.test('returns a matching querystring with default options for invalid delimiter', function (done) {
 
-            var output = nvpqs.stringify(object_stub, 'a');
+            var output = Nvpqs.stringify(ObjectStub, 'a');
 
             Code.expect(output).to.be.a.string();
-            Code.expect(output).to.equal(qs.stringify(nvp_stub));
+            Code.expect(output).to.equal(Qs.stringify(NVPStub));
 
             return done();
         });
 
         lab.test('returns a matching querystring using a custom separator character', function (done) {
 
-            var output = nvpqs.stringify(object_stub, null, '+');
+            var output = Nvpqs.stringify(ObjectStub, null, '+');
 
             Code.expect(output).to.be.a.string();
-            Code.expect(output).to.equal(qs.stringify(nvp_stub).replace(/\&/g, '+'));
+            Code.expect(output).to.equal(Qs.stringify(NVPStub).replace(/\&/g, '+'));
 
             return done();
         });
 
         lab.test('returns a matching querystring with default options for invalid separator', function (done) {
 
-            var output = nvpqs.stringify(object_stub, null, 'a');
+            var output = Nvpqs.stringify(ObjectStub, null, 'a');
 
             Code.expect(output).to.be.a.string();
-            Code.expect(output).to.equal(qs.stringify(nvp_stub));
+            Code.expect(output).to.equal(Qs.stringify(NVPStub));
 
             return done();
         });
 
         lab.test('returns a matching querystring using a custom assignment character', function (done) {
 
-            var output = nvpqs.stringify(object_stub, null, null, ':');
+            var output = Nvpqs.stringify(ObjectStub, null, null, ':');
 
             Code.expect(output).to.be.a.string();
-            Code.expect(output).to.equal(qs.stringify(nvp_stub).replace(/\=/g, ':'));
+            Code.expect(output).to.equal(Qs.stringify(NVPStub).replace(/\=/g, ':'));
 
             return done();
         });
 
         lab.test('returns a matching querystring with default options for invalid assignment character', function (done) {
 
-            var output = nvpqs.stringify(object_stub, null, null, 'a');
+            var output = Nvpqs.stringify(ObjectStub, null, null, 'a');
 
             Code.expect(output).to.be.a.string();
-            Code.expect(output).to.equal(qs.stringify(nvp_stub));
+            Code.expect(output).to.equal(Qs.stringify(NVPStub));
 
             return done();
         });
 
         lab.test('returns a matching querystring with default options for similiar custom characters', function (done) {
 
-            var output = nvpqs.stringify(object_stub, '.', '.', '.');
+            var output = Nvpqs.stringify(ObjectStub, '.', '.', '.');
 
             Code.expect(output).to.be.a.string();
-            Code.expect(output).to.equal(qs.stringify(nvp_stub));
+            Code.expect(output).to.equal(Qs.stringify(NVPStub));
 
             return done();
         });
