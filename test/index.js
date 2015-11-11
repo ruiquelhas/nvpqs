fae459f53b7d7bef6de083309f059b8f6ff20d15
Rui Quelhas <rfpquelhas@gmail.com>
2015-11-11 21:46:28 +0000

    Re-organize tests

--- test/index.js
+++ test/index.js
@@ -7,74 +7,75 @@ const Code = require('code');
 
 const lab = exports.lab = Lab.script();
 
-const Nvpqs = require('../');
+const nvpqs = require('../');
 
 const NVPStub = require('./stubs/nvp.json');
 const ObjectStub = require('./stubs/object.json');
 
-lab.experiment('NVP encoded querystring utility belt', () => {
+lab.experiment('nvpqs', () => {
 
-    lab.experiment('parses a querystring to proper JavaScript object and', () => {
+    lab.experiment('parse()', () => {
 
-        lab.test('returns empty object when the querystring is blank', (done) => {
+        lab.test('returns an empty object when the querystring is blank', (done) => {
 
-            const output = Nvpqs.parse('');
+            const output = nvpqs.parse('');
 
             Code.expect(output).to.be.an.object();
             Code.expect(output).to.be.empty();
-
             return done();
         });
 
-        lab.test('returns matching object when the querystring is not blank and valid', (done) => {
+        lab.test('returns a matching object when the querystring is not blank and valid', (done) => {
 
-            const output = Nvpqs.parse(Qs.stringify(NVPStub));
+            const output = nvpqs.parse(Qs.stringify(NVPStub));
 
             Code.expect(output).to.be.an.object();
             Code.expect(output).to.deep.equal(ObjectStub);
-
             return done();
         });
     });
 
-    lab.experiment('stringifies a JavaScript object to a proper querystring and', () => {
+    lab.experiment('stringify()', () => {
 
-        lab.test('returns a blank querystring if the object is empty or null', (done) => {
+        lab.test('returns a blank querystring if the object is null', (done) => {
 
-            const output = Nvpqs.stringify({});
+            const output = nvpqs.stringify(null);
 
             Code.expect(output).to.be.a.string();
             Code.expect(output).to.be.empty();
+            return done();
+        });
 
+        lab.test('returns a blank querystring if the object is empty', (done) => {
+
+            const output = nvpqs.stringify({});
+
+            Code.expect(output).to.be.a.string();
+            Code.expect(output).to.be.empty();
             return done();
         });
 
         lab.test('returns a matching querystring if the object is not empty nor null', (done) => {
 
-            const output = Nvpqs.stringify(ObjectStub);
+            const output = nvpqs.stringify(ObjectStub);
 
             Code.expect(output).to.be.a.string();
             Code.expect(output).to.equal(Qs.stringify(NVPStub));
-
             return done();
         });
-    });
-
-    lab.experiment('allows custom notitation delimiter and separator characters', () => {
 
         lab.test('returns a matching querystring using a custom delimiter character', (done) => {
 
-            const output = Nvpqs.stringify(ObjectStub, '-');
+            const output = nvpqs.stringify(ObjectStub, '-');
 
             Code.expect(output).to.be.a.string();
             Code.expect(output).to.equal(Qs.stringify(NVPStub).replace(/\./g, '-'));
-
             return done();
         });
 
         lab.test('returns a matching querystring with default options for invalid delimiter', (done) => {
 
-            const output = Nvpqs.stringify(ObjectStub, 'a');
+            const output = nvpqs.stringify(ObjectStub, 'a');
 
             Code.expect(output).to.be.a.string();
             Code.expect(output).to.equal(Qs.stringify(NVPStub));
@@ -84,7 +85,7 @@ lab.experiment('NVP encoded querystring utility belt', () => {
 
         lab.test('returns a matching querystring using a custom separator character', (done) => {
 
-            const output = Nvpqs.stringify(ObjectStub, null, '+');
+            const output = nvpqs.stringify(ObjectStub, null, '+');
 
             Code.expect(output).to.be.a.string();
             Code.expect(output).to.equal(Qs.stringify(NVPStub).replace(/\&/g, '+'));
@@ -94,7 +95,7 @@ lab.experiment('NVP encoded querystring utility belt', () => {
 
         lab.test('returns a matching querystring with default options for invalid separator', (done) => {
 
-            const output = Nvpqs.stringify(ObjectStub, null, 'a');
+            const output = nvpqs.stringify(ObjectStub, null, 'a');
 
             Code.expect(output).to.be.a.string();
             Code.expect(output).to.equal(Qs.stringify(NVPStub));
@@ -104,7 +105,7 @@ lab.experiment('NVP encoded querystring utility belt', () => {
 
         lab.test('returns a matching querystring using a custom assignment character', (done) => {
 
-            const output = Nvpqs.stringify(ObjectStub, null, null, ':');
+            const output = nvpqs.stringify(ObjectStub, null, null, ':');
 
             Code.expect(output).to.be.a.string();
             Code.expect(output).to.equal(Qs.stringify(NVPStub).replace(/\=/g, ':'));
@@ -114,7 +115,7 @@ lab.experiment('NVP encoded querystring utility belt', () => {
 
         lab.test('returns a matching querystring with default options for invalid assignment character', (done) => {
 
-            const output = Nvpqs.stringify(ObjectStub, null, null, 'a');
+            const output = nvpqs.stringify(ObjectStub, null, null, 'a');
 
             Code.expect(output).to.be.a.string();
             Code.expect(output).to.equal(Qs.stringify(NVPStub));
@@ -124,7 +125,7 @@ lab.experiment('NVP encoded querystring utility belt', () => {
 
         lab.test('returns a matching querystring with default options for similiar custom characters', (done) => {
 
-            const output = Nvpqs.stringify(ObjectStub, '.', '.', '.');
+            const output = nvpqs.stringify(ObjectStub, '.', '.', '.');
 
             Code.expect(output).to.be.a.string();
             Code.expect(output).to.equal(Qs.stringify(NVPStub));
